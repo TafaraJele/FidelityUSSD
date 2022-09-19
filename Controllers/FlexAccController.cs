d8c85d046b1cbb78277e0fdd17c8042f1b01db87
Tafara Jele <ext.tafara.jele@veneka.com>
2022-09-19 17:16:46 +0200

    Add create customer

---
+++ Controllers/FlexAccController.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Web.Http;
+using Veneka.Indigo.Abstractions.Models;
+using Veneka.Indigo.Core;
+namespace Indigo.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FlexAccController : ApiController
+    {
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <param name="customer"></param>
+       /// <returns></returns>
+        [HttpPost]
+
+        public FlexCustomerAccResp CreateFlexAccount([FromBody]FlexiCustomer customer)
+        {
+            Application application = new Application();                        
+
+            return application.CreateCustomer(customer); ;
+
+        }
+    }
+}
---
+++ Controllers/FlexCustController.cs
@@ -0,0 +1,38 @@
+﻿
+//using System.Web.Http;
+//using Veneka.Indigo.Abstractions.Models;
+
+
+//namespace Indigo.Controllers
+//{
+//    /// <summary>
+//    /// 
+//    /// </summary>
+//    public class FlexCustController : ApiController
+//    {
+
+//        /// <summary>
+//        /// /
+//        /// </summary>
+//        /// <param name="config"></param>
+//        /// <param name="customer"></param>
+//        /// <param name="msgid"></param>
+//        /// <param name="protocol"></param>
+//        /// <param name="address"></param>
+//        /// <param name="port"></param>
+//        /// <param name="path"></param>
+//        /// <param name="timeOutMilliseconds"></param>
+//        /// <param name="connectionString"></param>
+//        /// <returns></returns>
+//        [HttpPost]
+
+//        public CREATECUSTOMER_FSFS_RES PostCreateCustomerAccount(FlexcubeCreateCustomerConfig config, CREATECUSTOMER_FSFS_REQ customer, string msgid, Protocol protocol, string address, int port, string path, int? timeOutMilliseconds, string connectionString)
+//        {
+//            var customerValidation = new CustomerServiceValidated(protocol, address, port, path, timeOutMilliseconds, connectionString);
+//            var response = customerValidation.CreateCustomer(customer);
+
+//            return response;
+//        }
+
+//    }
+//}
